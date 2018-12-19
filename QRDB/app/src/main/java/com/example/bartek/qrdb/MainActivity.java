6dc5e2b37d50263c62842f9ac0d1fee7d6d8404c
Seler09 <blackbird092@gmail.com>
2018-12-19 12:31:35 +0100

    Add icon

--- QRDB/app/src/main/java/com/example/bartek/qrdb/MainActivity.java
+++ QRDB/app/src/main/java/com/example/bartek/qrdb/MainActivity.java
@@ -4,7 +4,6 @@ import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.icu.text.IDNA;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
